ca6a2ffff052af04055d27295710bb4b2a190ee7
Eric Charette <eric.charette@outlook.com>
2020-12-30 15:50:25 -0500

    redux in app and header components

--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -1,7 +1,10 @@
 import { Link } from "react-router-dom";
+import { connect } from "react-redux";
+
 import { auth } from "../../firebase/firebase.utils";
 
 import { ReactComponent as Logo } from "../../assets/crown.svg";
+
 import "./header.styles.scss";
 
 const Header = ({ currentUser }) => (
@@ -29,4 +32,8 @@ const Header = ({ currentUser }) => (
   </div>
 );
 
-export default Header;
+const mapStateToProps = (state) => ({
+  currentUser: state.user.currentUser,
+});
+
+export default connect(mapStateToProps)(Header);
