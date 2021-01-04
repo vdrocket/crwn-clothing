5c84c8b42b986d74e0b35463ce14fccc2defbf26
Eric Charette <eric.charette@outlook.com>
2021-01-04 10:54:18 -0500

    add some styled components

--- src/pages/homepage/homepage.component.jsx
+++ src/pages/homepage/homepage.component.jsx
@@ -1,11 +1,11 @@
 import Directory from "../../components/directory/directory.component";
 
-import "./homepage.styles.scss";
+import { HomePageContainer } from "./homepage.style";
 
 const HomePage = () => (
-  <div className="homepage">
+  <HomePageContainer>
     <Directory />
-  </div>
+  </HomePageContainer>
 );
 
 export default HomePage;
