5d8b265d629baac8dc2baf16d90bf529daa66d0d
Eric Charette <eric.charette@outlook.com>
2021-01-17 12:10:33 -0500

    convert to sagas

---
+++ src/redux/shop/shop.sagas.js
@@ -0,0 +1,37 @@
+import { takeLatest, call, put, all } from "redux-saga/effects";
+
+import {
+  firestore,
+  convertCollectionsSnapshotToMap,
+} from "../../firebase/firebase.utils";
+import {
+  fetchCollectionsSuccess,
+  fetchCollectionsFailure,
+} from "./shop.actions";
+
+import ShopActionTypes from "./shop.types";
+
+function* fetchCollectionsAsynch() {
+  try {
+    const collectionRef = firestore.collection("collections");
+    const snapshot = yield collectionRef.get();
+    const collectionsMap = yield call(
+      convertCollectionsSnapshotToMap,
+      snapshot
+    );
+    yield put(fetchCollectionsSuccess(collectionsMap));
+  } catch (error) {
+    yield put(fetchCollectionsFailure(error.message));
+  }
+}
+
+function* fetchCollectionsStart() {
+  yield takeLatest(
+    ShopActionTypes.FETCH_COLLECTIONS_START,
+    fetchCollectionsAsynch
+  );
+}
+
+export function* shopSagas() {
+  yield all([call(fetchCollectionsStart)]);
+}
